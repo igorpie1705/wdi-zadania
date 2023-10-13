f57f15b0c3af90e1032912fa40b86a84a1cf0363
Igoru <igorpie1705@gmail.com>
2023-10-13 18:54:52 +0200

    zad3

--- zad3.py
+++ zad3.py
@@ -1,5 +1,21 @@
-# Zadanie 3. Proszę napisać program sprawdzający czy istnieje spójny podciąg ciągu Fibonacciego o zadanej
-# sumie.
+# Zadanie 3. Proszę napisać program sprawdzający,
+# czy istnieje spójny podciąg ciągu Fibonacciego o zadanej sumie.
 
-def chuj():
-    pass
+def funkcja(suma):
+    a, b = 0, 1
+    tab = [a, b]
+    while b <= suma:
+        a, b = b, a + b
+        tab.append(b)
+        for i in range(0, len(tab)):
+            nowa_suma = 0
+            for j in range(i, len(tab)):
+                nowa_suma += tab[j]
+                if nowa_suma == suma:
+                    print("TAK")
+                    return 0
+    print("NIE")
+
+
+dane = int(input("Podaj sumę: "))
+funkcja(dane)
